4e009564445a0205a374ac53fe71581d788514a7
niketvjoshi <niketjoshi1987@gmail.com>
2023-08-06 09:15:07 +0530

    Committing Networking Components Setup

---
+++ vpc/module.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+  source                = "../modules/vpc"
+  vpc_01_cidr_block     = "10.102.0.0/16" 
+  subnet_01_cidr_block  = "10.102.10.0/24"  
+  subnet_02_cidr_block  = "10.102.20.0/24"
+}
---
+++ vpc/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  profile                       = "default"
+  #shared_credentials_files      = "../aws.credentials"
+  region                        = "ap-south-1" 
+}
