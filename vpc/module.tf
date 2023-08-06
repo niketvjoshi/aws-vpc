2761d70ecc97949ffd7a572684c230f5e2d4030d
niketvjoshi <niketjoshi1987@gmail.com>
2023-08-06 09:27:31 +0530

    Changed directory name

--- vpc/module.tf
+++ vpc/module.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                = "../modules/vpc"
+  source                = "../modules/aws_vpc"
   vpc_01_cidr_block     = "10.102.0.0/16" 
   subnet_01_cidr_block  = "10.102.10.0/24"  
   subnet_02_cidr_block  = "10.102.20.0/24"
